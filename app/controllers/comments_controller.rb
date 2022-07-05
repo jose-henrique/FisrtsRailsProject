db5a5381d563d0ef3430e0cdc3748a7d2a9bcab6
Francisco <franciscoeugenio@gmail.com>
2022-07-05 10:07:35 -0400

    Update comments_controller.rb
    fix mistyped @article variable on line #13

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
 
     def destroy
         @article = Article.find(params[:article_id])
-        @comment = @aticle.comments.find(params[:id])
+        @comment = @article.comments.find(params[:id])
         @comment.destroy
         redirect_to article_path(@article), status: 300
     end
